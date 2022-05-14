421f1457c787c3c99b4ae800a0f40b91ac5ea667
unknown <baldee229@gmail.com>
2022-05-14 23:16:01 +0300

    p2

--- assignment3/task2.php
+++ assignment3/task2.php
@@ -269,7 +269,7 @@
     </p>"; 
 
     echo "<p>
-    <h2>9.	Write a PHP script that creates the following table using for loops ?</h2>
+    <h2>10.	Write a PHP program that repeats integers from 1 to 50. For multiples of three, print \"Fizz\" instead of the number, and for multiples of five print \"Buzz\". For numbers that are multiples of both three and five, print \"FizzBuzz\" ?</h2>
         <p>";echo fzz(12, 4, 78) . "</p>
     </p>"; 
 ?>
